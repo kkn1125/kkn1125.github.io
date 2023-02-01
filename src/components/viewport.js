be67c7116ab90d297630742556743e2e266b1d0e
kkn1125 <xpfjrnseks1@naver.com>
2023-02-02 00:17:31 +0900

    gh-pages

--- src/components/viewport.js
+++ src/components/viewport.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+const scripts = `window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-179074259-1');`;
+
+
+
 export default function Viewport({ children }) {
+
   return (
     <Helmet>
       {/* To ensure proper rendering and touch zooming for all devices, add the responsive viewport meta tag to your <head> element. */}
@@ -12,6 +20,11 @@ export default function Viewport({ children }) {
       <meta name="naver-site-verification" content="6cebf2441529d02294b07c32ba2cd5ce09ba2c71" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7028508433313066"
         crossorigin="anonymous"></script>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-179074259-1"></script>
+      <script type="text/javascript">
+        {scripts}
+      </script>
       {children}
     </Helmet>
   );
